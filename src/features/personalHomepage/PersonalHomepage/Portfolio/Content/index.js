19aed68b90676551c27c483d1aa9cade34acf38d
Łukasz Susłowicz <susel.invest@gmail.com>
2025-12-17 16:06:28 +0100

    error view

---
+++ src/features/personalHomepage/PersonalHomepage/Portfolio/Content/index.js
@@ -0,0 +1,22 @@
+import { ErrorBox } from "./ErrorBox";
+import { Loading } from "./Loading";
+import { Repositories } from "./Repositories";
+
+export const Content = ({ status, repositories }) => {
+    switch (status) {
+        case "initial":
+            return null;
+
+        case "loading":
+            return <Loading />;
+
+        case "success":
+            return <Repositories repositories={repositories} />;
+
+        case "error":
+            return <ErrorBox />;
+            
+        default:
+            throw new Error(`Incorrect status: ${status}`);
+    }
+};
